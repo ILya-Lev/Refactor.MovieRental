21a31af2d7bd1c1ae20988a7000be4febb932289
tillileva <tillileva@SUBDN457>
2019-10-02 12:11:20 +0200

    extract subclass refactoring example

---
+++ SampleRefactorings/Initial/JobItem.cs
@@ -0,0 +1,32 @@
+﻿namespace SampleRefactorings.Initial
+{
+    /// <summary>
+    /// extract subclass sample
+    /// </summary>
+    public class JobItem
+    {
+        public int Quantity { get; }
+
+        private readonly decimal _unitPrice;
+        private readonly bool _isLabor;
+        private readonly Employee _employee;
+
+        public JobItem(decimal unitPrice, int quantity, bool isLabor, Employee employee)
+        {
+            _unitPrice = unitPrice;
+            Quantity = quantity;
+            _isLabor = isLabor;
+            _employee = employee;
+        }
+
+        public decimal GetTotalPrice() => GetUnitPrice() * Quantity;
+
+        private decimal GetUnitPrice() => _isLabor ? _employee.Rate : _unitPrice;
+    }
+
+    public class Employee
+    {
+        public decimal Rate { get; }
+        public Employee(decimal rate) => Rate = rate;
+    }
+}
---
+++ SampleRefactorings/Refactored/JobItem.cs
@@ -0,0 +1,32 @@
+﻿namespace SampleRefactorings.Refactored
+{
+    /// <summary>
+    /// extract subclass sample
+    /// </summary>
+    public abstract class JobItem
+    {
+        public int Quantity { get; }
+        protected JobItem(int quantity) => Quantity = quantity;
+        public abstract decimal GetTotalPrice();
+    }
+
+    internal class SpareItem : JobItem
+    {
+        public decimal UnitPrice { get; }
+        public SpareItem(decimal unitPrice, int quantity) : base(quantity) => UnitPrice = unitPrice;
+        public override decimal GetTotalPrice() => UnitPrice * Quantity;
+    }
+
+    public class LaborItem : JobItem
+    {
+        public Employee Employee { get; }
+        public LaborItem(int quantity, Employee employee) : base(quantity) => Employee = employee;
+        public override decimal GetTotalPrice() => Employee.Rate * Quantity;
+    }
+
+    public class Employee
+    {
+        public decimal Rate { get; }
+        public Employee(decimal rate) => Rate = rate;
+    }
+}
