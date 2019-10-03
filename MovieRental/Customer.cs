e20aa8d595d0f9d9d535f91ffa5df6747a98ba10
Illia Levandovskyi <lis2889@gmail.com>
2019-10-03 23:59:12 +0300

    protected visibility level changed to privagte protected one

--- MovieRental/Customer.cs
+++ MovieRental/Customer.cs
@@ -46,39 +46,39 @@ namespace MovieRental
             return report.ToString();
         }
 
-        protected abstract string CreateCaption(Customer customer);
-        protected abstract string CreateOneRentalMessage(Rental rental);
-        protected abstract string CreateChargeMessage(Customer customer);
-        protected abstract string CreatePointsMessage(Customer customer);
+        private protected abstract string CreateCaption(Customer customer);
+        private protected abstract string CreateOneRentalMessage(Rental rental);
+        private protected abstract string CreateChargeMessage(Customer customer);
+        private protected abstract string CreatePointsMessage(Customer customer);
     }
 
     internal class TextStatement : Statement
     {
-        protected override string CreateCaption(Customer customer)
+        private protected override string CreateCaption(Customer customer)
             => $"Rent report for {customer.Name}";
 
-        protected override string CreateOneRentalMessage(Rental rental)
+        private protected override string CreateOneRentalMessage(Rental rental)
             => $"\t{rental.Movie.Title}\t{rental.Charge}";
 
-        protected override string CreateChargeMessage(Customer customer)
+        private protected override string CreateChargeMessage(Customer customer)
             => $"Total debt is {customer.GetTotalCharge()}";
 
-        protected override string CreatePointsMessage(Customer customer)
+        private protected override string CreatePointsMessage(Customer customer)
             => $"You've earned {customer.GetFrequentRenterPoints()} activity points";
     }
 
     internal class HtmlStatement : Statement
     {
-        protected override string CreateCaption(Customer customer)
+        private protected override string CreateCaption(Customer customer)
             => $"<H1>Rent report for <EM>{customer.Name}</EM></H1><P>";
 
-        protected override string CreateOneRentalMessage(Rental rental)
+        private protected override string CreateOneRentalMessage(Rental rental)
             => $"{rental.Movie.Title}: {rental.Charge}<BR/>";
 
-        protected override string CreateChargeMessage(Customer customer)
+        private protected override string CreateChargeMessage(Customer customer)
             => $"<P>Total debt is <EM>{customer.GetTotalCharge()}</EM><P>";
 
-        protected override string CreatePointsMessage(Customer customer)
+        private protected override string CreatePointsMessage(Customer customer)
             => $"You've earned <EM>{customer.GetFrequentRenterPoints()}</EM> activity points<P>";
     }
 }
